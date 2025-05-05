12b648fe0383b8107de5a1516cdd1b937ac93626
HameezExE <hameezexe@gmail.com>
2025-05-05 17:56:46 +0530

    Added parellogram exercise

---
+++ C/1_C_Intro/Ex_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//use the printf function to print a parallelogram from "*"
+int main()
+{
+    //type your code below
+    
+    return 0;
+}
+
+// The parallegoram should look like this,
+
+// ****
+//  ****
+//   ****
+
+// Redo this after you have done for loops and while loops 
+
+// Redo this after you have learned how to give an input to the program
