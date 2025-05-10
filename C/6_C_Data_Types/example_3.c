60a6221f64549bf3d9832fd40e5cf7f4f8e5ef9a
keshi-xh <keshangeethanjana38@gmail.com>
2025-05-10 19:01:09 +0530

    added notes to example 3 in data types

--- C/6_C_Data_Types/example_3.c
+++ C/6_C_Data_Types/example_3.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+
 int main() {
-    char letter = 'B';
-    printf("Letter: %c\n", letter);
+    // A 'char' is a data type in C used to store a single character.
+    // It typically takes 1 byte of memory and can store any character from the ASCII table.
+    char letter = 'B'; // Here, we store the character 'B' in the variable 'letter'.
+
+    // Printing the character using the %c placeholder.
+    printf("Letter (using %%c): %c\n", letter);
+
+    // If we use the %d placeholder, it will print the ASCII value of the character.
+    printf("ASCII value (using %%d): %d\n", letter);
+
+    // Changing the character to demonstrate how it works.
+    letter = 'A';
+    printf("Updated Letter (using %%c): %c\n", letter);
+    printf("Updated ASCII value (using %%d): %d\n", letter);
+
     return 0;
 }
-//explain what a char is. 
-//store and print a character
-//also show what happens if we change the place holder
