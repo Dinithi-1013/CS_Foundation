350948b5832062ff6382479b5511893def74c00b
keshi-xh <keshangeethanjana38@gmail.com>
2025-05-10 18:42:44 +0530

    made changes to example one in variables

--- C/5_C_Variables/example_1.c
+++ C/5_C_Variables/example_1.c
@@ -6,8 +6,14 @@ int main()
     printf("x: %d\n", x);
     return 0;
 }
-// explain thoroughly
-// *how to declare and print them 
-// explain what %d is and the format used to use %d in a function 
-// explain that in data types place holders used to hold other data types will be explained
-// until that give that idea that the place holder used by an interger is %d
+
+// Notes:
+// - To declare an integer variable in C, use the `int` keyword followed by the variable name.
+//   Example: `int x = 15;` declares an integer variable `x` and initializes it with the value 15.
+// - To print the value of an integer variable, use the `printf` function with the `%d` format specifier.
+//   Example: `printf("x: %d\n", x);` prints the value of `x` followed by a newline.
+// - `%d` is a format specifier used in `printf` to represent an integer value.
+// - The format for using `%d` in `printf` is: `"text %d text", variable`.
+//   Replace `variable` with the name of the integer variable you want to print.
+// - Placeholders like `%d` are used to represent different data types in `printf`.
+//   For now, understand that `%d` is specifically for integers. Other placeholders for different data types will be explained later.
