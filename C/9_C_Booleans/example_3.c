3c002ce82b849e3e70fc88a78ea0330aa7d8e480
keshi-xh <keshangeethanjana38@gmail.com>
2025-05-24 18:29:15 +0530

    made some changes

--- C/9_C_Booleans/example_3.c
+++ C/9_C_Booleans/example_3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h> // Include the bool library
+
 int main() {
-    int x = 5, y = 10;
-    int both = x > 0 && y < 15;
+    int x = 5, y = 10; // Declare and initialize two integers
+
+    // Check if x is greater than 0 AND y is less than 15
+    bool both = (x > 0) && (y < 15);
+
+    // Print the result (1 for true, 0 for false)
     printf("Both true: %d\n", both);
-    return 0;
+
+    return 0; // End of program
 }
-
-
-// Remark:notice that the code is read from right to left always when the compiler reads the code
+/*
+ * Note:
+ * - The <stdbool.h> library is included to allow the use of the 'bool' type and boolean values 'true' and 'false' in C.
+ * - Booleans in C are represented as integers: 1 for true, 0 for false.
+ * - The variable 'both' stores the result of a logical AND operation between two conditions.
+ */
--- C/9_C_Booleans/example_4.c
+++ C/9_C_Booleans/example_4.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>      // Include standard input/output header for printf
+#include <stdbool.h>    // Include header for boolean type support
+
 int main() {
-    int x = 5, y = 10;
-    int both = x > 0 && y < 15;
+    int x = 5, y = 10;  // Declare and initialize two integer variables x and y
+    // Evaluate if x is greater than 0 AND y is less than 15, store result in 'both'
+    bool both = (x > 0) && (y < 15);
+    // Print the result: 1 if both conditions are true, 0 otherwise
     printf("Both true: %d\n", both);
-    return 0;
+    return 0;           // Indicate successful program termination
 }
