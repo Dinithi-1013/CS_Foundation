6093cde53995435b75f17e980ef302cc4b3e245d
keshi-xh <keshangeethanjana@gmail.com>
2025-06-08 19:51:39 +0530

    added example files for string handling in C

--- C/13_Arrays/Basics/example_1.c
+++ C/13_Arrays/Basics/example_1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+// Main function
 int main() {
-    int score[10];
+    int score[10]; // Declare an array to store 10 integers
+
     // Input 10 numbers
     for (int i = 0; i < 10; i++) {
-        printf("Enter score %d: ", i + 1);
-        scanf("%d", &score[i]);
+        printf("Enter score %d: ", i + 1); // Prompt user for input
+        scanf("%d", &score[i]); // Read input and store in array
     }
 
     // Display the array
     printf("The scores are: ");
     for (int i = 0; i < 10; i++) {
-        printf("%d ", score[i]);
+        printf("%d ", score[i]); // Print each score
     }
-    printf("\n");
+    printf("\n"); // Print newline
 
-    return 0;
+    return 0; // Indicate successful execution
 }
---
+++ C/14_Strings/example_1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+char *str = "Hello";
+// A string in C is a sequence of characters terminated bu a null character '\0'. 
+// It is essentially a pointer to the first element of a charater array
+
